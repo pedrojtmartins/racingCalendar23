31003f907446971a9c90ba02ff6661f8159e238b
Pedro Martins <pedrojtmartins@gmail.com>
2023-08-20 16:03:11 +0100

    Update library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0-beta02" apply false
+    id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.45" apply false
     alias(libs.plugins.com.android.library) apply false
